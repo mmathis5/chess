38f254e5556cb9a711cff7014f524861d87b43d6
Maddie Mathis <madelinemathis79@gmail.com>
2024-02-04 19:33:46 -0700

    implemented some of the silly functions in chess game

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,16 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private TeamColor turn;
     public ChessGame() {
-
+        this.turn = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return this.turn;
     }
 
     /**
@@ -27,7 +28,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.turn = team;
     }
 
     /**
