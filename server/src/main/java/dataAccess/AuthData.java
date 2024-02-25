3ceea39aa0c7c44171c8330476ae435947b68612
Maddie Mathis <madelinemathis79@gmail.com>
2024-02-24 23:19:14 -0700

    I think I had some files in the wrong place and I fixed that

---
+++ server/src/main/java/dataAccess/AuthData.java
@@ -0,0 +1,9 @@
+package dataAccess;
+
+public class AuthData {
+    private String authToken;
+    private String username;
+    public AuthData(){
+
+    }
+}
---
+++ server/src/main/java/dataAccess/UserData.java
@@ -0,0 +1,22 @@
+package dataAccess;
+
+public class UserData {
+    private String username;
+    private String password;
+    private String email;
+    public UserData(){
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
